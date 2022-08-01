b175ebed467bdc8b5dd6cb10f98756570fa3bbd7
zc <1544378095@qq.com>
2022-08-01 20:10:04 +0800

    hot-fix commit1

--- src/main/java/git/git_test.java
+++ src/main/java/git/git_test.java
@@ -6,5 +6,7 @@ package git;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello github");
+        System.out.println("hello zc");
     }
 }
